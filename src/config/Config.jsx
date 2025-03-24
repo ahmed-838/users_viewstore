c73b11d964d9b7d28bf9e0c273d16bf07747a7a1
ahmed-838 <ashams3719262@gmail.com>
2025-03-24 06:52:16 +0200

    adding error handlig and notifications

--- src/config/Config.jsx
+++ src/config/Config.jsx
@@ -1,5 +1,7 @@
 const Config = {
-  API_BASE_URL: 'http://192.168.156.68:4444'
+  // API_BASE_URL: 'http://192.168.156.68:4444',
+  API_BASE_URL: 'https://api.viewstore.shop'
+  
 };
 
 export default Config;
